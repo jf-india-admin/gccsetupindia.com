b2842dd7230d55c014d2cbc55b7a11038584c21a
ashish-awasthi-jf <88141651+ashish-awasthi-jf@users.noreply.github.com>
2025-09-23 15:35:36 +0530

    feat: add Resend Lambda handler and switchable LEADS_ENDPOINT flow

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 (function(){
   window.SITE_CONFIG = {
     GA4_ID: 'G-XXXXXXXXXX',
-    LINKEDIN_PARTNER_ID: '0000000',
-    HUBSPOT_PORTAL_ID: 'YOUR_PORTAL_ID',
-    HUBSPOT_FORM_ID: 'YOUR_FORM_ID',
-    CALENDLY_URL: 'https://calendly.com/'
+    LINKEDIN_PARTNER_ID: '',
+    HUBSPOT_PORTAL_ID: '',
+    HUBSPOT_FORM_ID: '',
+    CALENDLY_URL: 'https://calendly.com/',
+    LEADS_ENDPOINT: '' // e.g., https://your-amplify-domain.com/api/send-lead
   };
 })();
